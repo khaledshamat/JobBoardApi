837079fd836f2b9e4e3a226681940c41ee3d5969
Khaled Abu Shamat <khaledabushamat@khaleds-macbook-pro.home>
2018-10-20 20:01:51 +0300

    Posts CRUD

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,45 @@
+module Api
+	module V1
+		class PostsController < ApplicationController
+
+			def index
+				posts = Post.order('created_at DESC')
+				render json: {status: 'Success', message: 'Loaded Posts', data:posts}, status: :ok
+			end
+
+			def show
+				post = Post.find(params[:id])
+				render json: {status: 'Success', message: 'Loaded Post', data:post}, status: :ok
+			end
+
+			def create
+				post = Post.new(post_params)
+				if post.save
+					render json: {status: 'Success', message: 'Post Saved', data:post}, status: :ok
+				else
+					render json: {status: 'Success', message: 'Post Not Saved', data:post.errors}, status: :unprocessable_entity
+				end
+			end
+
+			def destroy
+				post = Post.destroy(params[:id])
+				render json: {status: 'Success', message: 'Post Deleted', data:post}, status: :ok
+			end
+
+			def update
+				post = Post.find(params[:id])
+				if post.update_attributes(post_params)
+					render json: {status: 'Success', message: 'Post Updated', data:post}, status: :ok
+				else
+					render json: {status: 'Success', message: 'Post Not Updated', data:post.errors}, status: :unprocessable_entity
+				end
+			end
+
+			private
+			def post_params
+				params.permit(:title, :body)
+			end
+
+		end
+	end
+end
